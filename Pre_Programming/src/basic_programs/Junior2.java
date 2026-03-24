a6f18a41c00b88fb25f89db907559bc0245a0292
sinch <sinch@Sinchana_21>
2026-03-24 22:40:44 +0530

    Jnior 2 commited

---
+++ Pre_Programming/src/basic_programs/Junior2.java
@@ -0,0 +1,10 @@
+package basic_programs;
+
+public class Junior2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Good Evening Everyone From Jnior 2");
+	}
+
+}
